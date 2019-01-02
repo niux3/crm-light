0af16b50484762f9f411f1dcd0dfeacd26b17f67
Niuxe <renaudbourdeau@gmail.com>
2019-01-02 20:49:33 +0100

    feat(log): add log in this system

--- src/emailing/Emailing.py
+++ src/emailing/Emailing.py
@@ -1,4 +1,5 @@
-import smtplib, random
+import smtplib, random, logging
+from logging.handlers import RotatingFileHandler
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from .Model import Model
@@ -9,6 +10,18 @@ class Emailing:
     @staticmethod
     def run():
         config = Configuration.get()
+
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+        file_handler = RotatingFileHandler(config['path']['log'], 'a', 1000000, 1)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.DEBUG)
+        logger.addHandler(stream_handler)
+
         model = Model()
         users = model.get(config['path']['data'])
         citations = model.get(config['path']['citations'])
@@ -30,4 +43,8 @@ class Emailing:
                 msg.attach(MIMEText(tpl.render(data), 'plain' if index == 0 else 'html'))
 
             server.sendmail(config['expediteur'], row['email'], msg.as_string().encode('utf-8'))
+            
+            ouputLog = "{c} {f} {l} ({e}) envoyé !"
+            logger.info(ouputLog.format(c = row['civilities'], f = row['firstname'], l = row['lastname'], e = row['email']))
+
         server.quit()
